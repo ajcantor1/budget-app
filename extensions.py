3bc97e0df76dee457b812f2fde58121d242bd23f
jeremy <ajcantor1@gmail.com>
2022-07-02 21:11:49 -0400

    ughhh

--- extensions.py
+++ extensions.py
@@ -1,7 +1,8 @@
 #extensions.py
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
 
-
+migrate = Migrate(app, db)
 jwt = JWTManager()
 db = SQLAlchemy()
--- factory.py
+++ factory.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from datetime import timedelta
 from api.routes import api
 from auth.routes import auth
-from extensions import jwt, db
+from extensions import jwt, db, migrate
 
 
 def createApp():
@@ -15,14 +15,15 @@ def createApp():
     app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET')   
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
-    dbName = os.getenv('DB_NAME')
+
 
     app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://vgwifshpalzzid:877ad5af646dbb085df34192c4990126c5d0a8a12a2458acca87dbfc485325bc@ec2-44-206-11-200.compute-1.amazonaws.com:5432/d2clpbpfggs58g'
     
     
     db.init_app(app)
     jwt.init_app(app)
-
+    migrate.init_app(app, db)
+    
     app.register_blueprint(auth, url_prefix='/auth')
     app.register_blueprint(api, url_prefix='/api')
 
