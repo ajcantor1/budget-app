93afefac9c49ce2f7d74e65b3a0d0c59697def27
jeremy <ajcantor1@gmail.com>
2022-07-02 20:59:21 -0400

    modified factory.py

--- factory.py
+++ factory.py
@@ -23,8 +23,6 @@ def createApp():
     db.init_app(app)
     jwt.init_app(app)
 
-    migrate = Migrate(app, db)
-
     app.register_blueprint(auth, url_prefix='/auth')
     app.register_blueprint(api, url_prefix='/api')
 
