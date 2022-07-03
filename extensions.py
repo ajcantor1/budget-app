b254e597e258c0a4c25598091c5c53b7a21a8f39
jeremy <ajcantor1@gmail.com>
2022-07-02 21:15:13 -0400

    ughhh

--- extensions.py
+++ extensions.py
@@ -3,6 +3,6 @@ from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
 
-migrate = Migrate(app, db)
+migrate = Migrate()
 jwt = JWTManager()
 db = SQLAlchemy()
