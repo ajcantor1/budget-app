cb0a5bb9c3d251c4d541e96e93612d71a20099af
jeremy <ajcantor1@gmail.com>
2022-07-03 14:57:17 -0400

    ughhh

--- factory.py
+++ factory.py
@@ -12,7 +12,7 @@ def createApp():
 
     app = Flask(__name__, template_folder='templates', static_folder='static')
 
-    app.config["JWT_SECRET_KEY"] = os.urandom(24)
+    app.config["JWT_SECRET_KEY"] = os.environ.get('SECRET_KEY')
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 
